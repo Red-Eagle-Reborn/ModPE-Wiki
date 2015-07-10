3f59900c19d820f192f7745c4ebd9326683a831a
respectZ <respectZ@users.noreply.github.com>
2015-07-10 14:59:58 +0700

    Update ModPE.js

--- ModPE.js
+++ ModPE.js
@@ -26,8 +26,40 @@ RER = {
 				}
 			}
 		}
+	},
+	allSword:[],
+	newSword:function(id,tex,data,name,damage,uses) {
+		ModPE.setItem(id,tex,data,name,damage,1);
+		Item.setMaxDamage(id,uses);
+		Item.setHandEquipped(id,!0);
+		RER.allSword.push([id,damage,uses])l
 	}
 }
-
-RER.chat("$4[RER]","Hellow World");
-RER.spawnMob(34,0,0,0);
+function attackHook(a,v) {
+	mainattackHook(a,v);
+	for(var i in RER.allSword) {
+		if(a==getPlayerEnt()) {
+			if(Player.getCarriedItem()==RER.allSword[i][0]) {
+				damageSword(RER.allSword[i][0],RER.allSword[i][2]);
+				Entity.setHealth(v,Entity.getHealth(v)-RER.allSword[i][1]);
+			}
+		}
+	}
+}
+function damageSword(id,max) {
+	var pcid = Player.getCarriedItemData();
+	if(pcid!=max) {
+		Entity.setCarriedItem(getPlayerEnt(),id,1,pcid+1);
+	} else {
+		Entity.setCarriedItem(getPlayerEnt(),0,0,0);
+		Level.playSound(Player.getX(),Player.getY(),Player.getZ(),"random.break",30,1)
+	}
+}
+function mainattackHook(a,v) {
+//Your code here
+}
+/*
+Examples : 
+RER.newSword(700,"sword",0,"Blazing Sword",7,500);
+RER.chat("[RER]","Hello World !");
+*/
