d0ded4dc377c1a175e7f1642c5cd15dba5665081
DevillordMCPE <maulaxe@yahoo.com>
2015-11-29 17:17:26 +0700

    Update ModPE.js

--- ModPE.js
+++ ModPE.js
@@ -1,7 +1,34 @@
 var readExternal = android.os.Environment.getExternalStorageDirectory();
 var musicPlayer = new android.media.MediaPlayer();
 
-RER = {
+R = {
+	x:function() {
+		getPlayerX();;
+	},
+	y:function() {
+		getPlayerY();
+	},
+	z:function() {
+		getPlayerZ();
+	},
+	playerEnt:function(m) {
+		getPlayerEnt();
+	},
+	mobsX:function(m) {
+		Entity.getX(m);
+	},
+	mobsY:function(m) {
+		Entity.getY(m);
+	},
+	mobsZ:function(m) {
+		Entity.getZ(m);
+	},
+	setPost:function(m,x,y,z) {
+		if(!R.isNull(m) && !R.isNull(x) && !R.isNull(y) && !R.isNull(z)) 
+			Entity.setPosition(m,x,y,z);
+		if(R.isNull(m) && !R.isNull(x) && !R.isNull(y) && !R.isNull(z))
+			Entity.setPosition(R.playerEnt(),x,y,z);
+	},
 	isNull:function(v) {
 		return v == null || v == 0 || v == undefined;
 	},
@@ -18,26 +45,26 @@ RER = {
 		musicPlayer.reset();
 	},
 	chat:function(prefix,str) {
-		if(RER.isNull(prefix)) 
+		if(R.isNull(prefix)) 
 			clientMessage(str);
 		else 
 			clientMessage(prefix,str);
 	},
 	spawnMob:function(ent,xx,yy,zz,skin) {
-		if(!RER.isNull(ent)) {
+		if(!R.isNull(ent)) {
 			if(xx == 0 || yy == 0 || zz == 0) {
-				if(RER.isNull(skin)) {
-					Level.spawnMob(ent,getPlayerX(),getPlayerY(),getPlayerZ());
+				if(R.isNull(skin)) {
+					Level.spawnMob(ent,R.x(),R.y(),R.z());
 				}
-				if(!RER.isNull(skin)) {
-					Level.spawnMob(ent,getPlayerX(),getPlayerY(),getPlayerZ(),skin);
+				if(!R.isNull(skin)) {
+					Level.spawnMob(ent,R.x(),R.y(),R.z(),skin);
 				}
 			}
 			if(xx != 0 || yy != 0 || zz != 0) {
-				if(RER.isNull(skin)) {
+				if(R.isNull(skin)) {
 					Level.spawnMob(ent,xx,yy,zz);
 				}
-				if(!RER.isNull(ent)) {
+				if(!R.isNull(ent)) {
 					Level.spawnMob(ent,xx,yy,zz,skin);
 				}
 			}
@@ -48,16 +75,16 @@ RER = {
 		ModPE.setItem(id,tex,data,name,damage,1);
 		Item.setMaxDamage(id,uses);
 		Item.setHandEquipped(id,!0);
-		RER.allSword.push([id,damage,uses]);
+		R.allSword.push([id,damage,uses]);
 	}
 }
 function attackHook(a,v) {
 	mainattackHook(a,v);
-	for(var i in RER.allSword) {
+	for(var i in R.allSword) {
 		if(a==getPlayerEnt()) {
-			if(Player.getCarriedItem()==RER.allSword[i][0]) {
-				damageSword(RER.allSword[i][0],RER.allSword[i][2]);
-				Entity.setHealth(v,Entity.getHealth(v)-RER.allSword[i][1]);
+			if(Player.getCarriedItem()==R.allSword[i][0]) {
+				damageSword(R.allSword[i][0],R.allSword[i][2]);
+				Entity.setHealth(v,Entity.getHealth(v)-R.allSword[i][1]);
 			}
 		}
 	}
@@ -68,7 +95,7 @@ function damageSword(id,max) {
 		Entity.setCarriedItem(getPlayerEnt(),id,1,pcid+1);
 	} else {
 		Entity.setCarriedItem(getPlayerEnt(),0,0,0);
-		Level.playSound(Player.getX(),Player.getY(),Player.getZ(),"random.break",30,1)
+		Level.playSound(R.x(),R.y(),R.z(),"random.break",30,1)
 	}
 }
 function mainattackHook(a,v) {
@@ -76,6 +103,6 @@ function mainattackHook(a,v) {
 }
 /*
 Examples : 
-RER.newSword(700,"sword",0,"Blazing Sword",7,500);
-RER.chat("[RER]","Hello World !");
+R.newSword(700,"sword",0,"Blazing Sword",7,500);
+R.chat("[R]","Hello World !");
 */
